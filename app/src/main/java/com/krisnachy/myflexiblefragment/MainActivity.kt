7c7dc9e2b3ba828dcd87f44fea4a6da784d03b7f
krisnachy <cahyayuana@gmail.com>
2024-02-24 14:36:04 +0700

    Initial commit

---
+++ app/src/main/java/com/krisnachy/myflexiblefragment/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.krisnachy.myflexiblefragment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val fragmentManager = supportFragmentManager
+        val homeFragment = HomeFragment()
+        val fragment = fragmentManager.findFragmentByTag(HomeFragment::class.java.simpleName)
+
+        if (fragment !is HomeFragment) {
+            Log.d("MyFlexibleFragment", "Fragment Name :" + HomeFragment::class.java.simpleName)
+        }
+    }
+}
